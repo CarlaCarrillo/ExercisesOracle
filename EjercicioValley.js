1a4807a711cfc4f170c7b45e901ea0053eaef1d5
CarlaCarrillo <49658506+CarlaCarrillo@users.noreply.github.com>
2019-07-05 14:45:29 -0500

    Add files via upload

---
+++ EjercicioValley.js
@@ -0,0 +1,12 @@
+//Se utiliza como string
+S ={U,D,D,D,U,D,U,U}
+//Se establece un avlor númerico para Up(U) y Down(D)
+var U = 1;
+var D =-1;
+//Se desarrolla función para contar número de valles
+function NumValles {
+let U = 1;
+let D =-1;
+  if ()
+} 
+console.log();
